9b9916d44ca92a87b490609916cc5a1b0b180584
fernandez-julian <julifersan@hotmail.com>
2018-10-15 15:36:47 -0300

    ClienteDAO + ConnectionFactory + excepcion de acceso y conexion

---
+++ API/src/excepciones/AccesoException.java
@@ -0,0 +1,11 @@
+package excepciones;
+
+public class AccesoException extends Exception {
+
+	private static final long serialVersionUID = 5190329702485382960L;
+
+	public AccesoException(String mensaje){
+		super(mensaje);
+	}
+	
+}
