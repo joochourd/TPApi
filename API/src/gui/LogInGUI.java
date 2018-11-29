8cd44919ecd4ad3dac09d6470a393b7054bba753
FlorSiri <florencia.belen@gmail.com>
2018-11-29 17:08:30 -0300

    Update LogInGUI.java

--- API/src/gui/LogInGUI.java
+++ API/src/gui/LogInGUI.java
@@ -1,39 +1,36 @@
 package gui;
 
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
-
-import clases.Empleado;
-import clases.Rol;
 import dao.EmpleadoDAO;
 import excepciones.AccesoException;
 import excepciones.ConexionException;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+
 import view.EmpleadoView;
 
-public class LogInGUI extends JFrame {
+public class LogInGUI {
 
-	private JPanel contentPane;
-	private JTextField txtNomUsr;
-	private JTextField txtContr;
+	private JFrame frame;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					LogInGUI frame = new LogInGUI();
-					frame.setVisible(true);
+					LogInGUI window = new LogInGUI();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -41,47 +38,54 @@ public class LogInGUI extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public LogInGUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 350, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setLayout(null);
 		
-		JLabel lblLogIn = new JLabel("Log In");
-		lblLogIn.setHorizontalAlignment(SwingConstants.CENTER);
-		lblLogIn.setBounds(187, 27, 82, 24);
-		contentPane.add(lblLogIn);
+		JLabel lblLogin = new JLabel("LogIn");
+		lblLogin.setFont(new Font("Dialog", Font.ITALIC, 18));
+		lblLogin.setBounds(146, 53, 70, 19);
+		frame.getContentPane().add(lblLogin);
 		
-		JLabel lblNombreDe = new JLabel("Nombre de Usuario:");
-		lblNombreDe.setBounds(127, 62, 106, 24);
-		contentPane.add(lblNombreDe);
+		JLabel lblNombreDeUsuario = new JLabel("Nombre de Usuario:");
+		lblNombreDeUsuario.setBounds(27, 109, 163, 15);
+		frame.getContentPane().add(lblNombreDeUsuario);
 		
-		JLabel lblContrasenia = new JLabel("Contrase\u00F1a:");
-		lblContrasenia.setBounds(127, 97, 70, 24);
-		contentPane.add(lblContrasenia);
+		JLabel lblContrasea = new JLabel("Contraseña:");
+		lblContrasea.setBounds(27, 153, 93, 15);
+		frame.getContentPane().add(lblContrasea);
 		
-		txtNomUsr = new JTextField();
-		txtNomUsr.setBounds(243, 62, 86, 20);
-		contentPane.add(txtNomUsr);
-		txtNomUsr.setColumns(10);
+		textField = new JTextField();
+		textField.setBounds(208, 107, 114, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
 		
-		txtContr = new JTextField();
-		txtContr.setBounds(243, 99, 86, 20);
-		contentPane.add(txtContr);
-		txtContr.setColumns(10);
+		passwordField = new JPasswordField();
+		passwordField.setBounds(208, 151, 114, 19);
+		frame.getContentPane().add(passwordField);
 		
-		JButton btnIniciarSesion = new JButton("Iniciar Sesi\u00F3n");
+		JButton btnIniciarSesion = new JButton("Iniciar Sesión");
+		btnIniciarSesion.setBounds(27, 222, 135, 25);
+		frame.getContentPane().add(btnIniciarSesion);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(197, 222, 125, 25);
+		frame.getContentPane().add(btnCancelar);
+	
 		btnIniciarSesion.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				EmpleadoDAO empDAO = new EmpleadoDAO();
 				empDAO.getInstancia();
 				try {
-					EmpleadoView emp = empDAO.buscarEmpleado(txtNomUsr.getText(), txtContr.getText());
+					@SuppressWarnings("deprecation")
+					EmpleadoView emp = empDAO.buscarEmpleado(textField.getText(), passwordField.getText());
 					if (emp != null) {
 						emp.getRolTemporal();
 						switch (emp.getRolTemporal()) {
@@ -113,21 +117,15 @@ public class LogInGUI extends JFrame {
 						System.out.println("Nombre de usuario incorrecto");
 					}
 				} catch (ConexionException | AccesoException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				} 
 			}
 		});
-		btnIniciarSesion.setBounds(127, 132, 95, 31);
-		contentPane.add(btnIniciarSesion);
 		
-		JButton btnCancelar = new JButton("Cancelar");
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
 			}
 		});
-		btnCancelar.setBounds(253, 130, 89, 33);
-		contentPane.add(btnCancelar);
-		
 	}
 }
